df464ed1e6594853305e086a381555cdbf7f8d07
Greg Look <greg@greg-look.net>
2016-07-27 16:13:08 -0700

    Add initial source with macro experiments.

---
+++ src/environ/sentry.clj
@@ -0,0 +1,46 @@
+(ns environ.sentry
+  (:require
+    [clojure.java.io :as io]
+    [clojure.tools.logging :as log]
+    [clojure.set :as set]
+    [environ.core :as e]))
+
+
+;; ## Var Declaration
+
+(def known-vars
+  "Map of environment keywords to a definition map which may contain a
+  `:description` and optionally a `:type` for auto-coercion."
+  {})
+
+
+(defn declare-env-var!
+  "Helper function for the `defenv` macro."
+  [env-key properties]
+  (when-let [extant (get known-vars env-key)]
+    (log/errorf "Environment variable definition for %s in %s:%d is overriding existing definition in %s:%d"
+                env-key (:ns properties) (:line properties) (:ns extant) (:line extant)))
+  (-> #'known-vars
+      (alter-var-root assoc env-key properties)
+      (get env-key)))
+
+
+(defmacro defenv
+  "Define a new environment variable used by the system."
+  [env-key description & {:as opts}]
+  `(declare-env-var!
+     ~env-key
+     (assoc ~opts
+            :ns '~(symbol (str *ns*))
+            :line ~(:line (meta &form))
+            :description ~description)))
+
+
+
+;; ## Sentry Behavior
+
+(def behavior
+  "Definition for how the sentry should behave in various situations."
+  {})
+
+; ...
---
+++ test/environ/sentry_test.clj
@@ -0,0 +1,9 @@
+(ns environ.sentry-test
+  (:require
+    [clojure.test :refer :all]
+    [environ.sentry :as sentry]))
+
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
