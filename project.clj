169a42cce91f9e10e41bac45d2b16252c871570c
Greg Look <greg@greg-look.net>
2016-10-03 12:20:53 -0700

    Add test and coverage profiles.

--- project.clj
+++ project.clj
@@ -4,11 +4,33 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
+  :aliases
+  {"coverage" ["with-profile" "+test,+coverage" "cloverage"]}
+
   :deploy-branches ["master"]
   :pedantic? :abort
+
+  :plugins
+  [[lein-codox "0.9.5"]
+   [rfkm/lein-cloverage "1.0.8"]]
 
   :dependencies
   [[org.clojure/clojure "1.8.0" :scope "provided"]
    [org.clojure/tools.logging "0.3.1"]
-   [environ "1.0.3"]
-   [table "0.5.0"]])
+   [environ "1.1.0"]
+   [table "0.5.0"]]
+
+  :codox
+  {:metadata {:doc/format :markdown}
+   :source-uri "https://github.com/amperity/envoy/blob/master/{filepath}#L{line}"
+   :doc-paths [""]
+   :output-path "doc/api"}
+
+  :profiles
+  {:test
+   {:dependencies [[commons-logging "1.2"]]
+    :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
+
+   :coverage
+   {:jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
+               "-Dorg.apache.commons.logging.simplelog.defaultlog=trace"]}})
