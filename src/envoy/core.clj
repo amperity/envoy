e42833674be1e635fc70d6e75bc6b8f267da8eac
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2016-12-28 11:32:09 -0800

    Update core.clj
    `set-env!` returns nil instead of entire env map

--- src/envoy/core.clj
+++ src/envoy/core.clj
@@ -258,4 +258,5 @@
   This should generally only be used from a REPL, and will not affect the actual
   system environment!"
   [var-key value & kvs]
-  (alter-var-root #'env #(apply assoc % var-key value kvs)))
+  (alter-var-root #'env #(apply assoc % var-key value kvs))
+  nil)
