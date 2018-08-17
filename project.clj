adce273404e0d7b0ea987d1e25f7e40bbf5a844d
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2018-08-17 14:09:00 -0700

    Bump versions of Clojure and clojure.tools.logging

--- project.clj
+++ project.clj
@@ -15,8 +15,8 @@
    [lein-cloverage "1.0.9"]]
 
   :dependencies
-  [[org.clojure/clojure "1.8.0" :scope "provided"]
-   [org.clojure/tools.logging "0.3.1"]
+  [[org.clojure/clojure "1.9.0" :scope "provided"]
+   [org.clojure/tools.logging "0.4.0"]
    [environ "1.1.0"]
    [table "0.5.0"]]
 
